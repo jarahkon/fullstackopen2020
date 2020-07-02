49861a9f113b6699c027b3ff28c02552ab349418
JokerRaiden <joona.rahkonen@gmail.com>
2020-07-02 15:38:40 +0300

    Refactoring for part 3

--- osa2/puhelinluettelo/src/App.js
+++ osa2/puhelinluettelo/src/App.js
@@ -1,69 +1,10 @@
 import React, { useState, useEffect } from 'react'
-import personService from '../services/persons'
+import personService from './services/persons'
+import Notification from './components/Notification'
+import Persons from './components/Persons'
+import Filter from './components/Filter'
+import PersonForm from './components/PersonForm'
 import './App.css'
-
-const Notification = ({ successMessage, errorMessage }) => {
-  if (!successMessage && !errorMessage) {
-    return null
-  }
-
-  if (successMessage) {
-    return (
-      <div className="success">
-        {successMessage}
-      </div>
-    )
-  }
-  else if (errorMessage) {
-    return (
-      <div className="error">
-        {errorMessage}
-      </div>
-    )
-  }
-}
-
-const Person = ({person, handleDelete}) => {
-  return (
-    <div>
-      {person.name} {person.number}
-      <button onClick={handleDelete}>delete</button>
-    </div>
-  )
-}
-
-const Persons = ({personsToShow, handleDelete}) => {
-  return (
-    <div>
-      {personsToShow.map( person => 
-      <Person key={person.name} person={person} handleDelete={() => handleDelete(person.name, person.id)} />
-    )}
-    </div>
-  )
-}
-
-const Filter = ({newFilter, handleNewFilter}) => {
-  return (
-    <div>
-      filter shown with <input value={newFilter} onChange={handleNewFilter} />
-    </div>
-  )
-}
-
-const PersonForm = ({ addPerson, newName, handleNewName, newNumber, handleNewNumber }) => {
-  return (
-    <form onSubmit={addPerson}>
-      <div>
-        name: <input value={newName} onChange={handleNewName}/>
-        <br/>
-        number: <input value={newNumber} onChange={handleNewNumber} />
-      </div>
-      <div>
-        <button type="submit">add</button>
-      </div>
-    </form>
-  )
-}
 
 const App = () => {
   const [ persons, setPersons ] = useState([])
---
+++ osa2/puhelinluettelo/src/components/PersonForm.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const PersonForm = ({ addPerson, newName, handleNewName, newNumber, handleNewNumber }) => {
+  return (
+    <form onSubmit={addPerson}>
+      <div>
+        name: <input value={newName} onChange={handleNewName}/>
+        <br/>
+        number: <input value={newNumber} onChange={handleNewNumber} />
+      </div>
+      <div>
+        <button type="submit">add</button>
+      </div>
+    </form>
+  )
+}
+
+export default PersonForm
