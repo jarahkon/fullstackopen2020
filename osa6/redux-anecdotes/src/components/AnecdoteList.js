0228648eb6705ac2cd04a447be6abf7ce9f9d841
JokerRaiden <joona.rahkonen@gmail.com>
2020-07-29 14:56:27 +0300

    6.12

--- osa6/redux-anecdotes/src/components/AnecdoteList.js
+++ osa6/redux-anecdotes/src/components/AnecdoteList.js
@@ -1,23 +1,36 @@
 import React from 'react'
 import { useSelector, useDispatch } from 'react-redux'
 import { voteAnecdote } from '../reducers/anecdoteReducer'
+import { showNotification, hideNotification } from '../reducers/notificationReducer'
 
 const AnecdoteList = (props) => {
   const anecdotes = useSelector(state => state.anecdotes)
+  const filter = useSelector(state => state.filter)
+
   const dispatch = useDispatch()
 
   const byVotes = (a1, a2) => a2.votes - a1.votes
 
+  const handleLike = (anecdote) => {
+    dispatch(voteAnecdote(anecdote.id))
+    dispatch(showNotification(`you voted '${anecdote.content}'`))
+    setTimeout(() => {
+      dispatch(hideNotification())
+    }, 5000)
+  }
+  
+  const filteredList = anecdotes.filter((anecdote) => anecdote.content.toLowerCase().includes(filter.toLowerCase()))
+
   return (
     <div>
-      {anecdotes.sort(byVotes).map(anecdote =>
+      {filteredList.sort(byVotes).map(anecdote =>
         <div key={anecdote.id}>
           <div>
             {anecdote.content}
           </div>
           <div>
             has {anecdote.votes}
-            <button onClick={() => dispatch(voteAnecdote(anecdote.id))}>vote</button>
+            <button onClick={() => handleLike(anecdote)}>vote</button>
           </div>
         </div>
       )}
--- osa6/redux-anecdotes/src/reducers/notificationReducer.js
+++ osa6/redux-anecdotes/src/reducers/notificationReducer.js
@@ -1,7 +1,24 @@
-const notificationReducer = (state = 'Hello world', action) => {
+const notificationReducer = (state = '', action) => {
   switch(action.type) {
+    case 'SHOW_NOTIFICATION':
+      return action.data.notification
+    case 'HIDE_NOTIFICATION':
+      return ''
     default:
       return state
+  }
+}
+
+export const showNotification = (notification) => {
+  return {
+    type: 'SHOW_NOTIFICATION',
+    data: { notification }
+  }
+}
+
+export const hideNotification = () => {
+  return {
+    type: 'HIDE_NOTIFICATION'
   }
 }
 
